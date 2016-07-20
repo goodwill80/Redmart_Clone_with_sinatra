faf924a73f81f060fc952ab7abd99ffa3d6b7018
kit jonathan ho <j.hokit80@gmail.com>
2016-07-20 22:54:15 +0800

    change table

--- app.rb
+++ app.rb
@@ -10,8 +10,32 @@ class Redmart_sinatraApp < Sinatra::Base
   end
 
   get '/users/:id' do
-      erb :'each_user'
+
   end
+
+  post "/users" do
+    # some code here
+  end
+
+
+  # PUT "/books/3" - Updates a specific book
+  put "/users/:id" do
+    # some code here
+  end
+
+  # PATCH "/books/3" - Partially updates a specific book
+  patch "/users/:id" do
+    # some code here
+  end
+
+  # DELETE "/books/3" - Deletes a specific book
+  delete "/users/:id" do
+    # some code here
+  end
+
+  get "/stylesheets/main.css" do
+  content_type 'text/css'
+end
 
 
 end
